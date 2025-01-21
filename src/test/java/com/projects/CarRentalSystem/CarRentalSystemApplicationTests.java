1ee5c976862e8d8c79f3e49e31a852cdfaa68652
Akshat Choudhary <choudharyakshat711@gmail.com>
2025-01-22 02:28:02 +0530

    Initial commit

---
+++ src/test/java/com/projects/CarRentalSystem/CarRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.CarRentalSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
